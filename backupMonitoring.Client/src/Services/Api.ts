593404403cee5ae70a4294709d4918e104d8c7ff
Ruhtra <kawanarthurskate@gmail.com>
2024-11-06 11:50:54 -0300

    Changes in ecosystemFile

--- backupMonitoring.Client/src/Services/Api.ts
+++ backupMonitoring.Client/src/Services/Api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+console.log(import.meta.env);
+
 export const api = axios.create({
-  baseURL: `http://localhost:8000/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   //   withCredentials: true,
 });
