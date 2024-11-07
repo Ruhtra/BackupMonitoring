1a50faa0d64d18b535aa70eb18d13d2ac57df419
Ruhtra <kawanarthurskate@gmail.com>
2024-11-07 09:40:25 -0300

    Finish config tailwind in vite-electron

---
+++ backupMonitoring.App/src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import Versions from './components/Versions'
+import electronLogo from './assets/electron.svg'
+
+function App(): JSX.Element {
+  return (
+    <>
+      <h1 className="bg-red-500">ohmaga</h1>
+    </>
+  )
+}
+
+export default App
