85f67713dfcf731ca0e2abb72d4d63781502a1d8
Ruhtra <kawanarthurskate@gmail.com>
2024-11-19 13:09:17 -0300

    Making a references projects and configure electron

--- backupMonitoring.Backup/src/main.ts
+++ backupMonitoring.Backup/src/main.ts
@@ -1,3 +1,9 @@
-export class a {
-  constructor() {}
+import { IUseCase } from "backupmonitoring.shared/Interfaces/IUseCase";
+export class BackupUseCase implements IUseCase<void, void> {
+  constructor() {
+    console.log("entrei");
+  }
+  execute(input: void): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
 }
